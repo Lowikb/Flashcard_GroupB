2850d7a039554ba941868fa2cc4a3f8843150713
Lowik BOURGEOISAT <lowik.bourgeoisat@geneanet.org>
2020-03-19 10:50:33 +0100

    There's now a video playing on main screen

--- app/src/main/java/com/lowik/flashcard_groupb/MainActivity.java
+++ app/src/main/java/com/lowik/flashcard_groupb/MainActivity.java
@@ -1,14 +1,60 @@
 package com.lowik.flashcard_groupb;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private VideoView videoBG;
+    MediaPlayer mMediaPlayer;
+    int mCurrentVideoPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        videoBG = (VideoView) findViewById(R.id.videoView);
+
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.groupbrally);
+
+        videoBG.setVideoURI(uri);
+        videoBG.start();
+
+        videoBG.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mMediaPlayer = mediaPlayer;
+                mMediaPlayer.setLooping(true);
+                if (mCurrentVideoPosition != 0) {
+                    mMediaPlayer.seekTo(mCurrentVideoPosition);
+                    mMediaPlayer.start();
+                }
+            }
+        });
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mCurrentVideoPosition = mMediaPlayer.getCurrentPosition();
+        videoBG.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        videoBG.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMediaPlayer.release();
+        mMediaPlayer = null;
+    }
+
 }
