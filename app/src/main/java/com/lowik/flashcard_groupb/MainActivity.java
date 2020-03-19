5288486e7d9bb667adb6433c2ac0b78dd2a33e48
Lowik BOURGEOISAT <lowik.bourgeoisat@geneanet.org>
2020-03-19 11:25:22 +0100

    Add about screen

--- app/src/main/java/com/lowik/flashcard_groupb/MainActivity.java
+++ app/src/main/java/com/lowik/flashcard_groupb/MainActivity.java
@@ -1,8 +1,11 @@
 package com.lowik.flashcard_groupb;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.VideoView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -33,6 +36,14 @@ public class MainActivity extends AppCompatActivity {
                     mMediaPlayer.seekTo(mCurrentVideoPosition);
                     mMediaPlayer.start();
                 }
+            }
+        });
+        Button aboutButton = findViewById(R.id.aboutButton);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
             }
         });
     }
