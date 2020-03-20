03ff1397661bdecd5b9e26c9e6fd0cb06f2c05b5
Lowik BOURGEOISAT <lowik.bourgeoisat@geneanet.org>
2020-03-20 12:21:35 +0100

    Add some question variants

--- app/src/main/java/com/lowik/flashcard_groupb/QuestionActivity.java
+++ app/src/main/java/com/lowik/flashcard_groupb/QuestionActivity.java
@@ -12,7 +12,9 @@ public class QuestionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // add a list
-        int[] layouts = new int[] {R.layout.activity_question0, R.layout.activity_question1, R.layout.activity_question2};
+        int[] layouts = new int[] {R.layout.activity_question0, R.layout.activity_question1,
+                R.layout.activity_question2, R.layout.activity_question3,
+                R.layout.activity_question4, R.layout.activity_question5};
         // randomly generate the layout
         setContentView(layouts[new Random().nextInt(layouts.length)]);
     }
