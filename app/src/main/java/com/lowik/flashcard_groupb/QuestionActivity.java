a1e8cbb09d465439226158dd72e34cfdad05b797
Lowik BOURGEOISAT <lowik.bourgeoisat@geneanet.org>
2020-03-20 11:53:23 +0100

    Change xml file name

--- app/src/main/java/com/lowik/flashcard_groupb/QuestionActivity.java
+++ app/src/main/java/com/lowik/flashcard_groupb/QuestionActivity.java
@@ -12,7 +12,7 @@ public class QuestionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // add a list
-        int[] layouts = new int[] {R.layout.activity_question, R.layout.activity_question1, R.layout.activity_question2};
+        int[] layouts = new int[] {R.layout.activity_question0, R.layout.activity_question1, R.layout.activity_question2};
         // randomly generate the layout
         setContentView(layouts[new Random().nextInt(layouts.length)]);
     }
