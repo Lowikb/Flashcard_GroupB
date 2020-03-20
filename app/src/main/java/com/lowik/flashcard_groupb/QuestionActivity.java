8dcad20f92a4614ef32e576121c09cfeccfc9044
Lowik BOURGEOISAT <lowik.bourgeoisat@geneanet.org>
2020-03-20 11:45:44 +0100

    Randomly generate the question

--- app/src/main/java/com/lowik/flashcard_groupb/QuestionActivity.java
+++ app/src/main/java/com/lowik/flashcard_groupb/QuestionActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Random;
+
 public class QuestionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question);
+        // add a list
+        int[] layouts = new int[] {R.layout.activity_question, R.layout.activity_question1, R.layout.activity_question2};
+        // randomly generate the layout
+        setContentView(layouts[new Random().nextInt(layouts.length)]);
     }
 }
